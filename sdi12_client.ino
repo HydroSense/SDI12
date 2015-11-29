f4541e56e2e93f8e62bae3fa7cd05e8b9a65ff8e
Colby Rome <cdr013@bucknell.edu>
2015-11-29 15:08:31 -0500

    first commit

---
+++ sdi12_client.ino
@@ -0,0 +1,16 @@
+/*
+   SDI-12 Client sketch
+
+   Responds to commands based on the SDI-12 v1.3 Specification.
+   The default sensor address is 'b'.
+
+   Colby Rome 11-29-2015
+*/
+
+void setup()
+{
+}
+
+void loop()
+{
+}
