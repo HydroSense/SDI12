57eefd85b0698f768ada188ecce81653f16ba52f
Colby Rome <cdr013@bucknell.edu>
2016-04-11 16:59:08 -0400

    Updated refresh to include wire.h for powering SDI bus

--- examples/refresh/refresh.ino
+++ examples/refresh/refresh.ino
@@ -4,6 +4,7 @@
 */
 
 #include "SDI.h"
+#include <Wire.h>
 
 #define SERIAL_OUTPUT_PIN 1
 #define FLOW_CONTROL_PIN A3
@@ -11,7 +12,29 @@
 SDIBusController *SDIBus;
 char addr;
 
+void powerSDIMiddlePort(){
+    // Powers the middle port on the Hydrosense Datalogger 2.1.2
+    pinMode(5, OUTPUT);
+    digitalWrite(5, HIGH);
+
+    Wire.begin();
+
+    Wire.beginTransmission(0b1110000);
+    Wire.write(byte(0x03));
+    Wire.write(0b00000000);  //Sets all pins to output
+    Wire.endTransmission();
+
+    Wire.beginTransmission(0b1110000);
+    Wire.write(byte(0x01));
+    Wire.write(0b00000100);  //Sets only Port2On. This is either mislabeled
+    // on the PDF, or incorrectly routed. Pin P2 is on which is incorrectly
+    // called Port3On
+    Wire.endTransmission();
+}
+
 void setup(){
+    powerSDIMiddlePort();
+
     // instantiate SDISerial instance with hardware Serial1
     pinMode(FLOW_CONTROL_PIN, OUTPUT);
     //pinMode(FLOW_CONTROL_PIN, OUTPUT);
@@ -54,9 +77,9 @@ void loop(){
         }
         else{
           Serial.println("Success. Read: ");
-          Serial.println(buffer[0]);
-          Serial.println(buffer[1]);
-          Serial.println(buffer[2]);
+          for(int i=0; i<numExpected; i++){
+            Serial.println(buffer[i]);
+          }
         }
 
     }
